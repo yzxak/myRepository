d720f5f12461cbd1aefacd57d4dfe5eae15a53aa
yzxak <1513006640@qq.com>
2023-03-01 15:23:48 +0800

    添加一行again

--- helloWord/src/cn/sy/Test.java
+++ helloWord/src/cn/sy/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我为什么这么帅哈哈嘻嘻");
         System.out.println("我为什么这么帅哈哈嘻嘻");
+        System.out.println("我为什么这么帅哈哈嘻嘻");
     }
 }
